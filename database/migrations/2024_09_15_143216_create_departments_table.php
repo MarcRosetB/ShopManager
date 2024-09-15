b69b67517912fcab91d27a45147e21330e90e837
Marc <m.roset.bermudez@gmail.com>
2024-09-15 16:36:00 +0200

    created department table

--- database/migrations/2024_09_15_143216_create_departments_table.php
+++ database/migrations/2024_09_15_143216_create_departments_table.php
@@ -4,16 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('departments', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+        Schema::create("departments", function (Blueprint $table) {
+            $table->id("department_id");
+            $table->string("department_name", 20);
         });
     }
 
@@ -22,6 +21,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('departments');
+        Schema::dropIfExists("departments");
     }
 };
