1c11192d5705b8b2348794cda87c5f9744200ae3
Marc <m.roset.bermudez@gmail.com>
2024-09-15 17:12:15 +0200

    created customers table

--- database/migrations/2024_09_15_144634_create_customers_table.php
+++ database/migrations/2024_09_15_144634_create_customers_table.php
@@ -4,16 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('customers', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+        Schema::create("customers", function (Blueprint $table) {
+            $table->id("customer_id");
+            $table->string("customer_name", 50);
+            $table->string("customer_address", 50);
+            $table->unsignedBigInteger("fk_town");
+            $table->char("customer_cif", 9);
+            $table->string("customer_email", 25)->unique();
+            $table->char("customer_phone", 9)->unique();
+
+            $table->foreign("fk_town")->references("town_id")->on("towns");
         });
     }
 
@@ -22,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('customers');
+        Schema::dropIfExists("customers");
     }
 };
