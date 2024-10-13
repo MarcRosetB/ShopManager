7ab4010af66f3a18e6f38720fbdb16443740e01d
Marc <m.roset.bermudez@gmail.com>
2024-10-13 12:45:45 +0200

    defines form registration routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EmployeeController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get("/", function () {
+    return view("welcome");
 });
+
+Route::get("/employees/create", [EmployeeController::class, "create"])->name(
+    "employees.create"
+);
+Route::post("/employees/store", [EmployeeController::class, "store"])->name(
+    "employees.store"
+);
