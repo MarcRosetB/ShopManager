69ec064284268c2286d5e490bc7fe4990134bdb7
Marc <m.roset.bermudez@gmail.com>
2025-10-05 22:20:57 +0200

    create employee table

--- InfrastructureLayer/DataBase/database.sql
+++ InfrastructureLayer/DataBase/database.sql
@@ -18,3 +18,15 @@ CREATE TABLE Department (
     DepartmentId INT AUTO_INCREMENT PRIMARY KEY,
     DepartmentName VARCHAR (50)
 );
+
+CREATE TABLE Employee (
+    EmployeeId INT AUTO_INCREMENT PRIMARY KEY,
+    EmployeeName VARCHAR (50) NOT NULL,
+    EmployeePhone CHAR (9) NOT NULL UNIQUE,
+    EmployeeEmail VARCHAR (50) NOT NULL UNIQUE,
+    EmployeePWD VARCHAR (255) NOT NULL,
+    Fk_Department INT NOT NULL,
+    Fk_Shop INT NOT NULL,
+    FOREIGN KEY (Fk_Department) REFERENCES Department(DepartmentId),
+    FOREIGN KEY (Fk_Shop) REFERENCES Shop(ShopId)
+);
