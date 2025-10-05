b993222142105c57e3f0f86501973a60d3c4af3a
Marc <m.roset.bermudez@gmail.com>
2025-10-05 22:41:45 +0200

    insert into department table

--- InfrastructureLayer/DataBase/inserts.sql
+++ InfrastructureLayer/DataBase/inserts.sql
@@ -5,3 +5,7 @@ INSERT INTO Town (TownName) VALUES ('Badalona');
 INSERT INTO Shop (ShopNickName, Fk_Town) VALUES ('Balmes', 1);
 INSERT INTO Shop (ShopNickName, Fk_Town) VALUES ('Ondina', 2);
 INSERT INTO Shop (ShopNickName, Fk_Town) VALUES ('Badalona', 3);
+
+INSERT INTO Department (DepartmentName) VALUES ('Ventas');
+INSERT INTO Department (DepartmentName) VALUES ('Contabilidad');
+INSERT INTO Department (DepartmentName) VALUES ('Montadores');
