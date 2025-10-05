4a9f22a0c435473e49024d1441d98b9cbb833ae1
Marc <m.roset.bermudez@gmail.com>
2025-10-05 22:39:17 +0200

    insert into shop table

--- InfrastructureLayer/DataBase/inserts.sql
+++ InfrastructureLayer/DataBase/inserts.sql
@@ -1,3 +1,7 @@
 INSERT INTO Town (TownName) VALUES ('Barcelona');
 INSERT INTO Town (TownName) VALUES ('Barbera del Valles');
 INSERT INTO Town (TownName) VALUES ('Badalona');
+
+INSERT INTO Shop (ShopNickName, Fk_Town) VALUES ('Balmes', 1);
+INSERT INTO Shop (ShopNickName, Fk_Town) VALUES ('Ondina', 2);
+INSERT INTO Shop (ShopNickName, Fk_Town) VALUES ('Badalona', 3);
